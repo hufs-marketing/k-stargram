78ffa83dd107ff6c9af3498af88e9dd4925cbf70
JINSUN <yoojis1234@naver.com>
2015-12-01 16:36:47 +0900

    make duplicating check about photo
    now we check the duplicated photos

--- photos.py
+++ photos.py
@@ -9,15 +9,15 @@ with open("client_info.txt", 'r') as f:
 
 api = InstagramAPI(access_token = access_token, client_secret=client_secret )
 dic = {}
-
+recent = {}
+flag = 1
 with open("userid_list.txt", 'r') as f:
     p = f.readlines()
 
 for i in p:
     user, name = i.split()[0], i.split()[1]
     dic[user] = {"name" : name, "photos_url" : []}
-
-
+    recent[user] = {"name" : name, "photos_url" : []}
 
 for k in dic.keys():
     recent_media, next_ = api.user_recent_media(user_id = k)
@@ -27,9 +27,40 @@ for k in dic.keys():
         else:
             dic[k]["photos_url"].append((media.images["standard_resolution"].url, None))
 
+r = open("recent_url.txt", 'r')
+u = open("upload_url.txt", 'w')
 for k in dic.keys():
     print "*" * 50
     print dic[k]["name"]
+    u.write(dic[k]["name"] + "\n")
+    r.readline()  # first line(name) read
+    line = r.readline() # second line(recent url) read
+    flag = 1
     for i in dic[k]["photos_url"]:
-        print i 
+        if line == (i[0] + "\n"):
+			#print "There are no new photos\n"
+            if flag == 1:
+                recent[k]["photos_url"] = i
+                flag = 0
+            break
+        print i
+        u.write(i[0] + "\n")
+        if flag == 1:
+            recent[k]["photos_url"] = i
+            flag = 0
+u.close() #upload_url close
+r.close() #recent_url close
+	
+f = open("recent_url.txt", 'w')
+
+for k in recent.keys():
+#   print "------------------------------------------------------"
+#   print recent[k]["name"]
+    f.write(recent[k]["name"] + "\n")#recent[k]["photos_url"][0] + "\n")
+#	f.write("\n")
+#    print recent[k]["photos_url"][0]
+    f.write(recent[k]["photos_url"][0] + "\n")
+
+f.close()
+
 
