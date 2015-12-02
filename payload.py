b1d53baf7c25c01aabcde5d61e34d58dd9b0d9ef
SunKyu <leesunkeuy@naver.com>
2015-12-02 17:05:38 +0900

    now we need to refactoring photo.py

--- payload.py
+++ payload.py
@@ -1,24 +1,13 @@
 # -*- coding:utf-8 -*-
-import requests
 import json
 import time
-import gdata
-from oauth_hook import OAuthHook
 
-
-def post_pic(name, img_url, google_id, comment=" "):
-    
-    client_id = ""
-    client_seceret = ""
-    site = "https://www.googleapis.com"
-
-    url = "https://www.googleapis.com/blogger/v3/blogs/485055035283972076/posts?fetchBody=true&fetchImages=true&isDraft=true&fields=author%2Cblog%2Ccontent%2Cimages%2Ctitle&key=Hywf81H5E3ydOzYwQ0LT-gTx"
+def make_body(name, img_url, google_id, comment=" "):
 
     img_tag = "<img src=\"%s\">" %(img_url)
     body = "%s \n %s"%(img_tag, comment)
     date = time.strftime("%Y-%m-%d")
     title = "%s, %s" %(name, date)
-
 
     payload = {
             "blog" : {"id" : "485055035283972076"},
@@ -26,14 +15,14 @@ def post_pic(name, img_url, google_id, comment=" "):
             "content" : body, 
             "title" : title,
             "kind": "blogger#post",
+            "labels" : [
+                tag
+                ]
             "images" : {
                 "url" : img_url
                 }
             }
-
-    r = requests.post(url, data = json.dumps(payload))
-    print r.text
-
+    return payload
 
 if __name__ == "__main__":
     url = "https://scontent.cdninstagram.com/hphotos-xat1/t51.2885-15/s640x640/sh0.08/e35/11348117_900427746699234_2027816491_n.jpg"
